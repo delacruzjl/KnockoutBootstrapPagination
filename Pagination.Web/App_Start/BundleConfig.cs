628623c461573271959bdedb105b0e41492906a9
Jose De la Cruz <jose@delacruzhome.com>
2016-11-20 02:00:27 -0500

    Includes bundling and nuget packages for dependencies.

---
+++ Pagination.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Optimization;
+
+namespace Pagination.Web {
+    public class BundleConfig {
+        public static void Register(BundleCollection bundles) {
+            bundles.UseCdn = true;
+
+            bundles.Add(new StyleBundle("~/content/normalize")
+                .Include("~/content/normalize.css"));
+
+            bundles.Add(new StyleBundle("~/content/bootstrap",
+                "//ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/css/bootstrap.css")
+                .Include("~/content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/content/site")
+                .Include("~/css/main.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr", "//ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.6.2.js")
+                .Include("~/scripts/modernizr-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "//ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js")
+                .Include("~/scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout", "//ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js")
+                .Include("~/scripts/knockout-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", "//ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/bootstrap.min.js")
+                .Include("~/scripts/knockout-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app")
+                .IncludeDirectory("~/js/", "*-ts.js", true));
+        }
+    }
+}
--- Pagination.Web/js/02-demo-viewmodel-ts.ts
+++ Pagination.Web/js/02-demo-viewmodel-ts.ts
@@ -1,22 +1,30 @@
-﻿interface IData {
+﻿enum PageStatus {
+    done, loading, errors
+}
+
+interface IData {
     Id: number,
     Text: string
 }
 
 interface IDemoViewModel {
     source: KnockoutObservableArray<IData>,
-    pagination: IPaginationViewModel
+    pagination: IPaginationViewModel,
+    pageStatus: KnockoutObservable<PageStatus>
 }
 
 module App {
     "use strict";
 
+
     class DemoViewModel implements IDemoViewModel {
         constructor() {
+            this.pageStatus = ko.observable(PageStatus.done);
             this.source = ko.observableArray([]);
             this.pagination = new PaginationViewModel(this.source.length, refreshData);
         }
 
+        pageStatus: KnockoutObservable<PageStatus>;
         source: KnockoutObservableArray<IData>;
         pagination: IPaginationViewModel;
     }
@@ -38,11 +46,13 @@ module App {
     //////////
 
     function refreshData(p?: number): void {
+        vm.pageStatus(PageStatus.loading);
         $.get("/app/data/?page=" + p,
-            (response) => {
-                vm.source(response.Rows);
-                var totalPages = Math.ceil(response.Total / vm.pagination.maxVisiblePages());
-                vm.pagination.totalPages(totalPages);
-            });
-    };
+        (response) => {
+            vm.source(response.Rows);
+            var totalPages = Math.ceil(response.Total / vm.pagination.maxVisiblePages());
+            vm.pagination.totalPages(totalPages);
+            vm.pageStatus(PageStatus.done);
+        });
+    }
 }
